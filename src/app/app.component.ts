d7b7efef89c7077523a1847f5fd6e566d082ca6a
Martín <magadiflo@gmail.com>
2022-08-29 13:31:14 -0500

    Subscribing to Observable 1 - Ejemplo de cómo funciona el observable y su subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+    //*Ejemplo del funcionamiento de un observable y subscribe
+    //*this.exampleObservable();
+  }
+
+  private exampleObservable(): void {
+    type HttpResponse = { code: number; data: string; };
+    const observable = new Observable<HttpResponse>(subscriber => {
+      console.log('Inside subscriber...');
+      subscriber.next({ code: 200, data: 'this is data 1...' });
+      subscriber.next({ code: 200, data: 'this is data 2...' });
+      subscriber.next({ code: 200, data: 'this is data 3...' });
+      //*subscriber.error({ code: 500, data: 'An error ocurred' });
+      setTimeout(() => {
+        subscriber.next({ code: 200, data: 'this is data more data...' });
+        subscriber.complete();
+      }, 3 * 1000);
+      console.log('Subscriber is done emitting...');
+    });
+
+    observable.subscribe({
+      next: (response: HttpResponse) => {
+        console.log('got response: ', response);
+      },
+      error: err => {
+        console.log('something wrong occurred: ', err);
+      },
+      complete: () => {
+        console.log('done!');
+      }
+    });
+  }
 
 }

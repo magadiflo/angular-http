b263092854d78730d575d3372c69a4ac629dfc55
Martín <magadiflo@gmail.com>
2022-08-30 09:24:48 -0500

    Subscribing to Observable 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {
-    //*Ejemplo del funcionamiento de un observable y subscribe
-    //*this.exampleObservable();
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    this.onGetUser();
   }
 
+  onGetUsers(): void {
+    this.userService.getUsers()
+      .subscribe({
+        next: users => console.log(users),
+        error: err => console.log(err),
+        complete: () => console.log('Done getting users')
+      });
+  }
+
+  onGetUser(): void {
+    this.userService.getUser()
+      .subscribe({
+        next: user => console.log(user),
+        error: err => console.log(err),
+        complete: () => console.log('Done getting user')
+      });
+  }
+
+  //*Ejemplo del funcionamiento de un observable y subscribe
   private exampleObservable(): void {
     type HttpResponse = { code: number; data: string; };
     const observable = new Observable<HttpResponse>(subscriber => {
